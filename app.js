2d86957cd01615c31213e9bf9f5a39d5cfe63b86
behnam <behnam.ghafouri@gmail.com>
2023-08-05 13:41:43 -0400

    Js Concepts

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-console.log('test test')
-var a = 1;
-var b = 2;
+cars = ['bmw', 'honda', 'benz', 'ford', 'sdfasdf', '324234', '21343124']
 
+
+
+let add = (event) => {
+    let newCar = document.getElementById("myInput").value
+    cars.push(newCar)
+    document.getElementById("myInput").value = ""
+    updateUi();
+}
+
+let deleteCar = (i) => {
+    cars = cars.filter((elm,index) => index != i)
+    updateUi();
+}
+
+let updateUi = () => {
+    htmlVariable = '<ul>';
+    cars.forEach((element,index) => {
+        console.log("element",element,"index",index)
+        htmlVariable += "<li onclick=deleteCar("+index +")>" + element + "</li>"
+    });
+    htmlVariable += '</ul>'
+    document.querySelectorAll("#root")[0].innerHTML = htmlVariable;
+}
+
+document.addEventListener("DOMContentLoaded", updateUi());
 
